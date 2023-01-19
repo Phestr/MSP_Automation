2cf2d6c41dd66a0af749a5d6e1609c980084dd40
Phestr <phestr@gmail.com>
2023-01-19 15:26:01 -0700

    update

--- Get-Eventlog.ps1
+++ Get-Eventlog.ps1
@@ -2,6 +2,6 @@
 #!/usr/local/bin/pws
 #EventLog designed to save log files to centralized admin drive
 #Eventlog parses Warning and Error system events for easy monitoring, while saving all events to a seperate log 
-Get-EventLog system -After (Get-Date).AddHours(-24) | Out-File -FilePath C:\User1_Logfiles\$(get-date -Format "yyyymmdd").AllLogs.txt
-Get-EventLog system -Entrytype Warning -After (Get-Date).AddHours(-24) | Out-File -FilePath C:\User1_Logfiles\$(get-date -Format "yyyymmdd").Warnings.txt
-Get-EventLog system -Entrytype Error -After (Get-Date).AddHours(-24) | Out-File -FilePath C:\User1_Logfiles\$(get-date -Format "yyyymmdd").Errors.txt
+Get-EventLog system -After (Get-Date).AddHours(-24) | Out-File -FilePath D:\User1_Logfiles\$(get-date -Format "yyyyMMdd").AllLogs.txt
+Get-EventLog system -Entrytype Warning -After (Get-Date).AddHours(-24) | Out-File -FilePath D:\User1_Logfiles\$(get-date -Format "yyyyMMdd").Warnings.txt
+Get-EventLog system -Entrytype Error -After (Get-Date).AddHours(-24) | Out-File -FilePath D:\User1_Logfiles\$(get-date -Format "yyyyMMdd").Errors.txt
